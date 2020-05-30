b2515317ea58f54f7bbd07e9f4cebdf61ae06753
Michael LaBarbera <michael.jlabarbera@gmail.com>
2020-05-29 19:54:52 -0700

    sprite physics and mechanics

--- src/src/Driver.java
+++ src/src/Driver.java
@@ -1,4 +1,150 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+import javax.swing.Timer;
 
-public class Driver {
+public class Driver extends JPanel implements ActionListener, KeyListener, MouseListener, MouseMotionListener {
+    private int screen_height = 800;
+    private int screen_width = 1000;
+    private Timer t;
+    private int stage = 0;
+
+    private AffineTransform tx;
+
+    Player player;
+
+    public void update(){
+        player.move();
+    }
+
+
+    public void paint(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.CYAN);
+        g.fillRect(0, 0, screen_width, screen_height);
+        g.setColor(Color.BLACK);
+        g.fillRect(475, 375, 50, 50); //placement, remove later
+
+        player.paint(g);
+    }
+
+    private Image getImage(String path){
+        Image tempImage = null;
+        try {
+            URL imageURL = null;
+            tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tempImage;
+    }
+
+    public void startGame(){
+
+    }
+
+    /////////////////////////////////////////////// DRIVER ///////////////////////////////////////////
+
+    public static void main(String[] args) {
+        Driver d = new Driver();
+    }
+
+    public Driver() {
+        JFrame f = new JFrame();
+        f.setTitle("Yariscopter");
+        f.setSize(screen_width, screen_height);
+        f.setResizable(false);
+        f.addKeyListener(this);
+        f.addMouseListener(this);
+
+        //sprite instantiation
+        player = new Player("yarisright.png");
+
+        f.add(this);
+        t = new Timer(10, this);
+        t.start();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+
+    public boolean isInside(int x, int y, int xBound1, int yBound1, int xBound2, int yBound2) {
+        if(x >= xBound1 && x <= xBound2 && y >= yBound1 && y <= yBound2) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        update();
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        switch(key){
+            case 38:
+                player.up();
+                break;
+            case 40:
+                player.down();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        switch(key){
+            case 38:
+            case 40:
+                player.neutral();
+                break;
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
--- src/src/Player.java
+++ src/src/Player.java
@@ -1,4 +1,96 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class Player {
+
+    //instance variables
+    private double x, y;				//position in map
+    private double vx, vy;			//velocity
+    private double ax, ay;			//acceleration
+    private double angle;			//current flight angle
+    private double rv;	    		//rotation velocity
+    private double appliedThrust;	//current forward thrust from yaris
+
+    //display variables
+    private double scale = 0.25;					//size of yaris on screen
+    private double displayX = 50, displayY = 400;	//display location for yaris (the center)
+    private double offsetx = (int)(540*scale);		//offset of yaris image (to center yaris)
+    private double offsety = (int)(390*scale);
+    private int width = (int)(1024*scale);		//size of yaris image
+    private int height= (int)(768*scale);
+    private Image img;							//image
+
+    //physics variables
+    private int mass = 100;
+    private double lift = 0;
+    private double gravity = 0.15;
+    private double power = 1;
+
+    //gameplay variables
+    private boolean onGround = false;			//keep track if yaris dies
+    public boolean alive;						//aliveness of our manual 2007 red yaris driving friend
+    public Shape bounds;						//collision boundaries for yaris (for obstacles and ground)
+    public boolean onRamp;
+
+    //affine
+    private AffineTransform tx;
+    private String path;
+
+    public Player(String fileName){
+        x = 0;
+        y = 0;
+        angle = 0;
+        vx = 5;
+        vy = 0;
+        rv = 0;
+        img = getImage(fileName);
+        alive = true;
+        tx = AffineTransform.getTranslateInstance(displayX, displayY - offsety);
+        tx.scale(scale, scale);
+        bounds = new Rectangle();
+    }
+
+    public void move(){
+        updateAccelerations();
+        //double fy =
+        vy -= ay;
+        displayY += vy;
+        tx.setToTranslation(displayX, displayY - offsety);
+        tx.scale(scale, scale);
+    }
+
+    private void updateAccelerations() {
+        double fy = lift - gravity;
+        ay = fy / mass;
+    }
+
+    public void up(){
+        lift += power;
+    }
+
+    public void down(){
+        lift -= power;
+    }
+    public void neutral(){
+        lift = 0;
+    }
+    public void paint(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(img, tx, null);
+        g2.setColor(Color.BLACK);
+    }
+
+    private Image getImage(String path){
+        this.path = path;
+        Image tempImage = null;
+        try {
+            URL imageURL = Player.class.getResource(path);
+            tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tempImage;
+    }
+
 }
